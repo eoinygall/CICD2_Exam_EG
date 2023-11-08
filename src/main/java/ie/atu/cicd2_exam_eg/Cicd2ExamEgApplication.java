f245978bb6e9c5e2a4ab4847b35f2077d24406bd
G00392003 <G00392003@atu.ie>
2023-11-08 09:02:41 +0000

    Initial commit

---
+++ src/main/java/ie/atu/cicd2_exam_eg/Cicd2ExamEgApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd2_exam_eg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd2ExamEgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd2ExamEgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd2_exam_eg/Cicd2ExamEgApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd2_exam_eg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2ExamEgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
